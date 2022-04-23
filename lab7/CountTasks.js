25d79ead4305339b889149ab302ef012be79a445
hklees <hklees@knights.ucf.edu>
2022-04-23 15:37:47 -0400

    April 23, 2022 3:37 PM

--- lab7/CountTasks.js
+++ lab7/CountTasks.js
@@ -1,10 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function CountTasks () {
     const [count, setcount] = useState(0);
     useEffect(() => {
       document.title = `You have completed ${count} task(s)!`;
     });
+    return (
+        <div>
+            <p> You completed {count} tasks.</p>
+        </div>
+    )
   }
 
   export default CountTasks;
