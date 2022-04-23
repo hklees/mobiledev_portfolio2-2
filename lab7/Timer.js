422df1ebe36ee1aa47fda84d393635c07c7fa4c7
hklees <hklees@knights.ucf.edu>
2022-04-23 14:30:08 -0400

    April 23, 2022 2:30 PM

--- lab7/Timer.js
+++ lab7/Timer.js
@@ -1,30 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 
 //new functional component #1
+<View style={styles.timestyle}>
+
+</View>
 const Timer = () => {
     const [seconds, setSeconds] = useState(0);
     const [isActive, setIsActive] =useState(true);
-
+    
     function onOffSwith (){
         setIsActive(!isActive)
     }
-
+    
     //if statement needs to go here
     useEffect(() => {
         let interval = null;
-    if (isActive) {
-        interval =setInterval(() => {
-            setSeconds(seconds => seconds +1);
-        }, 1000); //1000= = 1 second
-    } else if (isActive & seconds !==0) {
-        clearInterval(interval);
-    }
-    return () => clearInterval(interval);
-}, [isActive, seconds]);
-
-return (
-<View style={styles.timestyle}>
+        if (isActive) {
+            interval =setInterval(() => {
+                setSeconds(seconds => seconds +1);
+            }, 1000); //1000= = 1 second
+        } else if (isActive & seconds !==0) {
+            clearInterval(interval);
+        }
+        return () => clearInterval(interval);
+    }, [isActive, seconds]);
+    
+    return (
+        <View style={styles.timestyle}>
 <div className="timediv">
 
     <div className="time">
