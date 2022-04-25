faf658593412abe2052f749bf1a34035dacc3b59
hklees <hklees@knights.ucf.edu>
2022-04-25 10:33:16 -0400

    April 25, 2022 10:33 AM

--- lab7/Notes.js
+++ lab7/Notes.js
@@ -1,19 +1,29 @@
 import React, { useState } from 'react';
 
 //user can input notes in box?
-export default() => {
+const Notes = props => props.data.map(note => <div>{note.text}</div>);
+
+export default () => {
     //const stuff here
     const initialData = [{text: 'Enter extra notes here'}];
     const [data, setData] = useState(initialData);
+    const handleClick = () => {
+        const text = document.querySelector('#noteinput').value.trim();
+        if (text) {
+            //const here
+        };
+        document.querySelector('#noteinput').value = '';
+        setData(nextState);
+    }
 
-}
 
-return (
-    <>
-    <input id='notesinput' type="text" placeholder="Enter notes here" />
-    //button to sumbit note input
-    <button onClick={() => handleClick()}>Add note</button>
-    <Notes data={data} />
-    </>
-);
+    return (
+        <>
+        <input id='notesinput' type="text" placeholder="Enter notes here" />
+        {/* button to sumbit note input */}
+        <button onClick={() => handleClick()}>Add note</button>
+        <Notes data={data} />
+        </>
+    );
 };
+
